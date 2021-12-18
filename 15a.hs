af2e761fe0762c2fe346b77a557dae0e3c827fc5
Antti Pham <antti.pham@gmail.com>
2021-12-18 17:25:57 +0200

    not efficient enough

--- 15a.hs
+++ 15a.hs
@@ -1,20 +1,24 @@
+import qualified Data.Set as S
 import Data.Char (digitToInt)
 main = do
     inputStr <- getContents
     let
         arr = parse inputStr
         maxIndex = length arr - 1
-        result = dp arr maxIndex maxIndex
+        result = calcRisk arr S.empty maxIndex maxIndex
     print result
 
 parse :: String -> [[Int]]
 parse s = map (map digitToInt) $ lines s
 
-dp :: [[Int]] -> Int -> Int -> Int
-dp arr i j
-    | i < 0 || j < 0   = maxBound
-    | i == 0 && j == 0 = 0
-    | otherwise        = curVal + minPrev
+calcRisk :: [[Int]] -> S.Set (Int, Int) -> Int -> Int -> Int
+calcRisk arr past i j
+    | i == 0 && j == 0                         = 0
+    | i < 0 || j < 0 || i >= size || j >= size = maxBound
+    | (i, j) `S.member` past                   = maxBound
+    | otherwise                                = curVal + minPrev
     where
+        size = length arr
         curVal = arr !! i !! j
-        minPrev = min (dp arr (i-1) j) (dp arr i (j-1))
+        calc = calcRisk arr $ S.insert (i, j) past
+        minPrev = minimum [calc (i-1) j, calc i (j-1), calc (i+1) j, calc i (j+1)]
