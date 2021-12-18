08975552417885bf6998dc64e26a5568ef184301
Antti Pham <antti.pham@gmail.com>
2021-12-18 23:13:14 +0200

    easy when I understand the problem...

---
+++ 15b.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=500, ERR=-1;
+vector<string> arr;
+bool visited[N][N] = {};
+int n = 0;
+multimap<int, pair<int,int>> cur;
+
+int get_arr(int i, int j) {
+    if (i < 0 || j < 0 || i > n || j > n) return ERR;
+    return arr[i][j] - '0';
+}
+
+void visit(int prev_val, int i, int j) {
+    int risk_val = get_arr(i, j);
+    if (risk_val == ERR) return;
+    if (visited[i][j]) return;
+
+    int cur_val = prev_val + risk_val;
+    cur.insert({cur_val, {i,j}});
+}
+
+int dijkstra() {
+    cur.insert({0, {0,0}});
+
+    while (cur.size()) {
+        auto cur_it = cur.begin();
+        auto p = cur_it->second;
+        auto [i,j] = p;
+        cur.erase(cur_it);
+
+        if (visited[i][j]) continue;
+        visited[i][j] = 1;
+
+        if (i==n && j==n) {
+            return cur_it->first;
+        }
+        auto val = cur_it->first;
+        visit(val, i-1, j);
+        visit(val, i+1, j);
+        visit(val, i, j-1);
+        visit(val, i, j+1);
+
+    }
+    return 0;
+}
+
+string increase_risk(string s, int n) {
+    for (auto&& c : s) {
+        c+=n;
+        while (c > '9') c-=9;
+    }
+    return s;
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string ss;
+    while (cin >> ss) {
+        arr.push_back(ss);
+    }
+
+    auto orig = arr;
+    for (int n=1; n<5; n++) {
+        for (int i=0; i<orig.size(); i++) {
+            arr[i]+=increase_risk(orig[i], n);
+        }
+    }
+    orig = arr;
+    for (int n=1; n<5; n++) {
+        for (int i=0; i<orig.size(); i++) {
+            arr.push_back(increase_risk(orig[i], n));
+        }
+    }
+
+
+    n = arr.size() - 1;
+    cout << dijkstra() << "\n";
+}
